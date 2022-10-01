9f6c2a75dc03c440abe9d77c165216e3733ed41d
IvanFitiskin <ifitiskin93@gmail.com>
2022-10-01 12:46:13 +0300

    test for constructor

---
+++ src/test/java/ConstructorTest.java
@@ -0,0 +1,68 @@
+import org.junit.Test;
+import pages.ConstructorPage;
+import pages.HomePage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ConstructorTest {
+
+    @Test
+    public void openSauceBoardTest() {
+        open("https://stellarburgers.nomoreparties.site");
+
+        HomePage homePage = new HomePage();
+        homePage.waitLoading();
+
+        ConstructorPage constructorPage = new ConstructorPage();
+        constructorPage.clickSauceButton();
+
+        boolean isSauceButtonSelected = constructorPage.isSelectedSauceButton();
+        assertTrue("sauce button is not selected", isSauceButtonSelected);
+
+        boolean isSelectedBunButton = constructorPage.isSelectedBunButton();
+        boolean isSelectedFillingButton = constructorPage.isSelectedFillingButton();
+        assertFalse("bun button is selected", isSelectedBunButton);
+        assertFalse("filling button is selected", isSelectedFillingButton);
+    }
+
+    @Test
+    public void openBunBoardTest() {
+        open("https://stellarburgers.nomoreparties.site");
+
+        HomePage homePage = new HomePage();
+        homePage.waitLoading();
+
+        ConstructorPage constructorPage = new ConstructorPage();
+        constructorPage.clickSauceButton()
+                .clickBunButton();
+
+        boolean isSelectedBunButton = constructorPage.isSelectedBunButton();
+        assertTrue("bun button is not selected", isSelectedBunButton);
+
+        boolean isSauceButtonSelected = constructorPage.isSelectedSauceButton();
+        boolean isSelectedFillingButton = constructorPage.isSelectedFillingButton();
+        assertFalse("sauce button is selected", isSauceButtonSelected);
+        assertFalse("filling button is selected", isSelectedFillingButton);
+    }
+
+    @Test
+    public void openFillingBoardTest() {
+        open("https://stellarburgers.nomoreparties.site");
+
+        HomePage homePage = new HomePage();
+        homePage.waitLoading();
+
+        ConstructorPage constructorPage = new ConstructorPage();
+        constructorPage.clickFillingButton();
+
+        boolean isSelectedFillingButton = constructorPage.isSelectedFillingButton();
+        assertTrue("filling button is not selected", isSelectedFillingButton);
+
+        boolean isSauceButtonSelected = constructorPage.isSelectedSauceButton();
+        boolean isSelectedBunButton = constructorPage.isSelectedBunButton();
+        assertFalse("sauce button is selected", isSauceButtonSelected);
+        assertFalse("bun button is selected", isSelectedBunButton);
+    }
+}
